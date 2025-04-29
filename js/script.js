2af3d3e37295dbaba83e0b294348abc3b8c4df68
Timupas <164215517+Timupas@users.noreply.github.com>
2025-04-29 19:53:33 +0200

    dz7

--- js/script.js
+++ js/script.js
@@ -1,36 +1,36 @@
 const numbers = [1, 2, 3];
 numbers[1] = 10;
-console.log("1:", numbers);
+console.log("Змінений масив чисел:", numbers);
 
 const strings = ["one", "two", "three"];
 strings.push("four");
-console.log("2:", strings);
+console.log("Масив рядків після додавання:", strings);
 
 const numsForSum = [5, 10, 15];
 const sum = numsForSum.reduce((acc, curr) => acc + curr, 0);
-console.log("3: Сума =", sum);
+console.log("Сума чисел у масиві:", sum);
 
 const nums5 = [1, 2, 3, 4, 5];
-console.log("4:");
+console.log("Всі елементи масиву з 5 чисел:");
 for (let i = 0; i < nums5.length; i++) {
   console.log(nums5[i]);
 }
 
 const strArray = ["timur", "frontend", "goiteens", "js", "script"];
-console.log("5:");
-for (const str of strArray) {
-  if (str.length > 5) {
-    console.log(str);
+console.log("Рядки довші за 5 символів:");
+for (let i = 0; i < strArray.length; i++) {
+  if (strArray[i].length > 5) {
+    console.log(strArray[i]);
   }
 }
 
 const nums10 = [3, 17, 8, 22, 1, 14, 9, 33, 4, 19];
 const max = Math.max(...nums10);
-console.log("6: Максимальне число =", max);
+console.log("Максимальне число в масиві:", max);
 
-console.log("7: Парні числа:");
-for (const n of nums10) {
-  if (n % 2 === 0) {
-    console.log(n);
+console.log("Парні числа з масиву:");
+for (let i = 0; i < nums10.length; i++) {
+  if (nums10[i] % 2 === 0) {
+    console.log(nums10[i]);
   }
 }
